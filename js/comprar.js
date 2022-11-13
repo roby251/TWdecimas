393add7b4616e9cc115cd2a4ba44f72931db3786
roby251 <bastian.mamani.yucra@alumnos.uta.cl>
2022-11-13 14:57:03 -0300

    mejorar botones de articulos

--- js/comprar.js
+++ js/comprar.js
@@ -1,3 +1,15 @@
 function recargar(){
     document.getElementById("sueldo").innerHTML = 250000; 
 }
+function agregarArticulo(articulo){
+    if (document.getElementById(articulo).innerHTML =="agregado"){
+        document.getElementById(articulo).innerHTML = "Comprar"; 
+        document.getElementById(articulo).className = "btn btn-info";
+    }
+    else{
+        document.getElementById(articulo).innerHTML = "agregado"; 
+        document.getElementById(articulo).className = "btn btn-warning";
+
+    }
+
+}
