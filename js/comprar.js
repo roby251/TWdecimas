1b14897f805d9a5068175d24eca1793da41559eb
roby251 <bastian.mamani.yucra@alumnos.uta.cl>
2022-11-13 19:20:49 -0300

    quitar detalles que no pude :(

--- js/comprar.js
+++ js/comprar.js
@@ -5,9 +5,6 @@ function agregarArticulo(boton,articulo){
     const precioN =document.getElementById(articulo).innerHTML
     const precioP = precioN.slice(1) // quitar el simbolo de precio
     if (document.getElementById(boton).innerHTML =="agregado"){
-        document.getElementById(boton).innerHTML = "Comprar"; 
-        document.getElementById(boton).className = "btn btn-info";
-        document.getElementById("pagar").innerHTML =  parseInt(document.getElementById("pagar").innerHTML)-parseInt(precioP); 
     }
     else{
         document.getElementById(boton).innerHTML = "agregado"; 
@@ -18,8 +15,6 @@ function agregarArticulo(boton,articulo){
 
 }
 function asegurarcompra(){
-    const all = document.getElementsByClassName("btn btn-warning");
-    alert(all.totring());
 
 
     if (parseInt(document.getElementById("sueldo").innerHTML)-parseInt(document.getElementById("pagar").innerHTML)>0){
