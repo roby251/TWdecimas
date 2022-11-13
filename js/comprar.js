69abc29801671009e52d3746f094342e66934c81
roby251 <bastian.mamani.yucra@alumnos.uta.cl>
2022-11-13 19:16:09 -0300

    tode

--- js/comprar.js
+++ js/comprar.js
@@ -1,15 +1,32 @@
 function recargar(){
     document.getElementById("sueldo").innerHTML = 250000; 
 }
-function agregarArticulo(articulo){
-    if (document.getElementById(articulo).innerHTML =="agregado"){
-        document.getElementById(articulo).innerHTML = "Comprar"; 
-        document.getElementById(articulo).className = "btn btn-info";
+function agregarArticulo(boton,articulo){
+    const precioN =document.getElementById(articulo).innerHTML
+    const precioP = precioN.slice(1) // quitar el simbolo de precio
+    if (document.getElementById(boton).innerHTML =="agregado"){
+        document.getElementById(boton).innerHTML = "Comprar"; 
+        document.getElementById(boton).className = "btn btn-info";
+        document.getElementById("pagar").innerHTML =  parseInt(document.getElementById("pagar").innerHTML)-parseInt(precioP); 
     }
     else{
-        document.getElementById(articulo).innerHTML = "agregado"; 
-        document.getElementById(articulo).className = "btn btn-warning";
+        document.getElementById(boton).innerHTML = "agregado"; 
+        document.getElementById(boton).className = "btn btn-warning";
+        document.getElementById("pagar").innerHTML =  parseInt(document.getElementById("pagar").innerHTML)+parseInt(precioP); 
 
     }
 
+}
+function asegurarcompra(){
+    const all = document.getElementsByClassName("btn btn-warning");
+    alert(all.totring());
+
+
+    if (parseInt(document.getElementById("sueldo").innerHTML)-parseInt(document.getElementById("pagar").innerHTML)>0){
+        document.getElementById("sueldo").innerHTML=parseInt(document.getElementById("sueldo").innerHTML)-parseInt(document.getElementById("pagar").innerHTML); 
+        document.getElementById("pagar").innerHTML =0;
+    }
+    else{
+        alert("ERROR");
+    }
 }
