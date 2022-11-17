a61cc60a5c68df9d4ffa19b11b36c3a54fa96b97
roby251 <106534644+roby251@users.noreply.github.com>
2022-11-17 12:36:26 -0300

    opcion cancelar compra

--- js/comprar.js
+++ js/comprar.js
@@ -4,10 +4,14 @@ function recargar(){
 function agregarArticulo(boton,articulo){
     const precioN =document.getElementById(articulo).innerHTML
     const precioP = precioN.slice(1) // quitar el simbolo de precio
-    if (document.getElementById(boton).innerHTML =="agregado"){
+    if (document.getElementById(boton).innerHTML =="Agregado"){
+        document.getElementById(boton).innerHTML = "Comprar"; 
+        document.getElementById(boton).className = "btn btn-info";
+        document.getElementById("pagar").innerHTML =  parseInt(document.getElementById("pagar").innerHTML)-parseInt(precioP); 
+
     }
-    else{
-        document.getElementById(boton).innerHTML = "agregado"; 
+    else if(document.getElementById(boton).innerHTML =="Comprar"){
+        document.getElementById(boton).innerHTML = "Agregado"; 
         document.getElementById(boton).className = "btn btn-warning";
         document.getElementById("pagar").innerHTML =  parseInt(document.getElementById("pagar").innerHTML)+parseInt(precioP); 
 
@@ -20,7 +24,11 @@ function asegurarcompra(){
     if (parseInt(document.getElementById("usaldo").innerHTML)-parseInt(document.getElementById("pagar").innerHTML)>=0){
         document.getElementById("usaldo").innerHTML=parseInt(document.getElementById("usaldo").innerHTML)-parseInt(document.getElementById("pagar").innerHTML); 
         document.getElementById("pagar").innerHTML =0;
-        
+        var elemento = document.getElementsByClassName('btn btn-warning'); 
+        for (var i = 0; i < elemento.length; i++) {  
+            document.getElementById(elemento[i].getAttribute('id')).innerHTML = "Comprado"; 
+            document.getElementById(elemento[i].getAttribute('id')).className = "btn btn-danger";
+          }
         var x = document.getElementById("apruebop");
         if (x.style.display === "none") {
             x.style.display = "block";
